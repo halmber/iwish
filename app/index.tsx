08231762b2d9f03467a2ec007ec4ca6c3da990a8
halmber <rogozyanskiy.bogdan@gmail.com>
2024-11-27 00:10:00 +0200

    feat: reset project and add nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-[#1e1f35]">
+      <Text className="text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
