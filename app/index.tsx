0e648e6bf9a5ad51c21fccec0bc0edf42db4955a
halmber <rogozyanskiy.bogdan@gmail.com>
2024-11-30 00:10:02 +0200

    feat: configure firebase config with env variables

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,8 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-[#1e1f35]">
       <Text className="text-white">
-        Edit app/index.tsx to edit this screen.
+        Edit app/index.tsx to edit this screen. See firebase project id:{" "}
+        {process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID}
       </Text>
     </View>
   );
