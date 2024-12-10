19a3de608a31d4fee30ed6ceb9c1936523cfcef0
halmber <rogozyanskiy.bogdan@gmail.com>
2024-12-10 23:28:09 +0200

    fix(NotFoundScreen): text color

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,14 +1,15 @@
+import { Text } from "@/components/ui";
 import { Link, Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <View className="flex-1 bg-[#1e1f35] align-middle justify-center p-20">
+      <View className="flex-1 bg-[#1e1f35] align-middle justify-center items-center">
         <Text>This screen doesn't exist.</Text>
         <Link href="/" className="mt-4 px-4">
-          <Text>Go to home screen!</Text>
+          <Text className="text-[#ff6347]">Go to home screen!</Text>
         </Link>
       </View>
     </>
